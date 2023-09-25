95cd12c9a9c010f3952c272663122df7567df277
AbhitanshRIL <abhitansh.gupta@ril.com>
2023-09-25 17:07:04 +0530

    html method

--- src/app/moderation/moderation.component.ts
+++ src/app/moderation/moderation.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-moderation',
   templateUrl: './moderation.component.html',
   styleUrls: ['./moderation.component.css']
 })
-export class ModerationComponent {
-
+export class ModerationComponent{
+  selectedImage !: string;
+  imageOptions: string[] = ['../../assets/Bkg (1).png', '../../assets/image1.jpg', '../../assets/hm5.jfif']; // Replace with your image URLs
+  imageDetails: { [key: string]: string } = {
+    'image1.jpg': 'Image 1 Description',
+    'image2.jpg': 'Image 2 Description',
+    'image3.jpg': 'Image 3 Description'
+  };
 }
