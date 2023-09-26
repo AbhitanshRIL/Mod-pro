af5eda465839c7a5274f71ce9cf8689d1bc23d01
AbhitanshRIL <abhitansh.gupta@ril.com>
2023-09-26 11:34:33 +0530

    dropdown button

--- src/app/moderation/moderation.component.ts
+++ src/app/moderation/moderation.component.ts
@@ -9,8 +9,24 @@ export class ModerationComponent{
   selectedImage !: string;
   imageOptions: string[] = ['../../assets/Bkg (1).png', '../../assets/image1.jpg', '../../assets/hm5.jfif']; // Replace with your image URLs
   imageDetails: { [key: string]: string } = {
-    'image1.jpg': 'Image 1 Description',
-    'image2.jpg': 'Image 2 Description',
-    'image3.jpg': 'Image 3 Description'
+    '../../assets/Bkg (1).png': 'Image 1 Description',
+    '../../assets/image1.jpg': 'Image 2 Description',
+    '../../assets/hm5.jfif': 'Image 3 Description'
   };
+
+  //dropdown
+  dropdownOptions: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  selectedOption: string = '';
+  isDropdownOpen: boolean = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isDropdownOpen = false;
+  }
+
+
 }
