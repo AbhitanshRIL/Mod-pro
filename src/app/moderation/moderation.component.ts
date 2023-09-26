5244701194c351680c0d7df97c4ac08b46f06b01
Harshvardhan1 P <Harshvardhan1.P@ril.com>
2023-09-26 12:52:13 +0530

    minor changes

--- src/app/moderation/moderation.component.ts
+++ src/app/moderation/moderation.component.ts
@@ -13,7 +13,7 @@ export class ModerationComponent{
   POA:string = '../../assets/Bkg (1).png';
   POI:string= '../../assets/image1.jpg';
   Doc1:string = '../../assets/hm5.jfif';
-  selectedImage !: string;
+  selectedImage =this.POI;
   imageOptions: string[] = [this.POA,this.POI,this.Doc1]; // Replace with your image URLs
   imageDetails: { [key:string]: string } = {
     [this.POA] : 'Address Proof',
@@ -21,18 +21,11 @@ export class ModerationComponent{
     [this.Doc1] : 'Document'
   };
 
-  //dropdown
-  dropdownOptions: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-  selectedOption: string = '';
-  isDropdownOpen: boolean = false;
+ 
 
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  selectOption(option: string) {
-    this.selectedOption = option;
-    this.isDropdownOpen = false;
+  ngOnInit() {
+    console.log("testing by harsh");
+    console.log(this.selectedImage);
   }
 
 
