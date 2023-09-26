3aece9ad1d23b334d3de1af6b5cf46d4f0793eaf
Harshvardhan1 P <Harshvardhan1.P@ril.com>
2023-09-26 16:42:47 +0530

    Monal's Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from '@angular/forms';
+import { ModerationFormComponent } from './moderation-form/moderation-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModerationComponent
+    ModerationComponent,
+    ModerationFormComponent
   ],
   imports: [
     BrowserModule,
