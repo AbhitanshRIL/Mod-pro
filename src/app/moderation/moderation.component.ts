2de43e2b487208ca2e0ad17230db86db87ec8d46
AbhitanshRIL <abhitansh.gupta@ril.com>
2023-09-26 16:45:38 +0530

    merged

--- src/app/moderation/moderation.component.ts
+++ src/app/moderation/moderation.component.ts
@@ -13,27 +13,13 @@ export class ModerationComponent{
   POA:string = '../../assets/Bkg (1).png';
   POI:string= '../../assets/image1.jpg';
   Doc1:string = '../../assets/hm5.jfif';
-  selectedImage !: string;
+  selectedImage : string = this.POI;
   imageOptions: string[] = [this.POA,this.POI,this.Doc1]; // Replace with your image URLs
   imageDetails: { [key:string]: string } = {
     [this.POA] : 'Address Proof',
     [this.POI] : 'Identity Proof',
     [this.Doc1] : 'Document'
   };
-
-  //dropdown
-  dropdownOptions: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
-  selectedOption: string = '';
-  isDropdownOpen: boolean = false;
-
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  selectOption(option: string) {
-    this.selectedOption = option;
-    this.isDropdownOpen = false;
-  }
 
 
 }
