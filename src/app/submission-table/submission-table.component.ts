aa1b038d18b49b0604b868a94e6cd4ca4c533590
Riyona Lasrado <riyona.lasrado.jio@gmail.com>
2023-09-28 14:53:09 +0530

    View button in table

--- src/app/submission-table/submission-table.component.ts
+++ src/app/submission-table/submission-table.component.ts
@@ -2,23 +2,18 @@ import {Component} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
   position: number;
-  weight: number;
+  name: string;
+  // weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 101011, name: 'George Max', symbol: 'H'},
+  {position: 202202, name: 'Zee Carol',  symbol: 'He'},
+  {position: 303030, name: 'Ravi Ken', symbol: 'Li'},
+  {position: 440044, name: 'Mary Louis', symbol: 'Be'},
+  {position: 505050, name: 'Harry Rock', symbol: 'B'}
 ];
 
 @Component({
@@ -29,6 +24,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   imports: [MatTableModule],
 })
 export class SubmissionTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'symbol'];
   dataSource = ELEMENT_DATA;
 }
