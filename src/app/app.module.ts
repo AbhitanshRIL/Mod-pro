55cc93b04015389a78882f49af5bc4cffcdc1103
Monal Mahajan <Monal.Mahajan@ril.com>
2023-09-29 10:57:52 +0530

    KYC improvised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatTableModule } from '@angular/material/table';
 import { ModerationFormComponent } from './moderation-form/moderation-form.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
+
 @NgModule({
   declarations: [
     AppComponent,
