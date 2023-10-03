d801d0355fc7261e3d7f576fa9b529798e3d69bc
Riyona Lasrado <riyona.lasrado.jio@gmail.com>
2023-10-03 17:07:26 +0530

    KYC latest update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,14 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { KycFormComponent } from './kyc-form/kyc-form.component';
+// import { KycFormComponent } from './kyc-form/kyc-form.component';
 
 
 
